9ae361181ff136875b38a12b8cfbb0b3688a6370
Alena Egorova <anon5007@gmail.com>
2016-04-09 12:27:27 +0300

    Update Matrix.h

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -25,26 +25,26 @@ private:
 	int m_rows;
 
 public:
-	CMatrix() : m_rows(0), m_columns(0), matrix(nullptr) {}; // Done
-	CMatrix(unsigned int n, unsigned int m); // Done
+	CMatrix() : m_rows(0), m_columns(0), matrix(nullptr) {};	// Done
+	CMatrix(unsigned int n, unsigned int m);					// Done
 	CMatrix(T **matr, unsigned int _n, unsigned int _m);
 	CMatrix(CMatrix const & x);
 	~CMatrix();
 
-	unsigned int rowsNumber() const; // Done
-	unsigned int columnsNumber() const; // Done
-	bool readFromFile(char *path); // Done
+	unsigned int rowsNumber() const;							// Done
+	unsigned int columnsNumber() const;							// Done
+	bool readFromFile(char *path);								// Done
 
 	CMatrix &operator =(const CMatrix& m2);
-	CMatrix operator +(const CMatrix& m2); // Done
-	CMatrix operator -(const CMatrix& m2); // Done
-	CMatrix operator *(double num); // Done
-	CMatrix operator *(const CMatrix& m2); // Done
-	bool operator ==(const CMatrix& m2); // Done
-	T* operator [](unsigned int index); // Done
+	CMatrix operator +(const CMatrix& m2);						// Done
+	CMatrix operator -(const CMatrix& m2);						// Done
+	CMatrix operator *(double num);								// Done
+	CMatrix operator *(const CMatrix& m2);						// Done
+	bool operator ==(const CMatrix& m2);						// Done
+	T* operator [](unsigned int index);							// Done
 
-	friend std::ostream & operator<< <>(std::ostream & output, const CMatrix & matrix); // Done
-	friend std::istream & operator>> <>(std::istream & input, CMatrix<T> & matrix); // Done
+	friend std::ostream & operator<< <>(std::ostream & output, const CMatrix & matrix);	// Done
+	friend std::istream & operator>> <>(std::istream & input, CMatrix<T> & matrix);		// Done
 };
 
 
